3f315d49fbbdf90b8c4153d78c6a54dd6994d941
Rafael Niebles <rnieblesealo@gmail.com>
2024-10-22 12:01:37 -0400

    progress

--- mysh.c
+++ mysh.c
@@ -10,26 +10,73 @@
 
 int main() {
   char cmd_buffer[BUF_SIZE];
-  char *cmd_tokens[MAX_ARGS];
-  
+  char **tokens;
+  int token_c;
+  int i, j;
+
   while (1) {
     printf("$ ");
 
-    // get whole command
+    // get whole command, clean newlines
     fgets(cmd_buffer, BUF_SIZE, stdin);
     cmd_buffer[strcspn(cmd_buffer, "\n")] = '\0';
 
-    // tokenize by space
-    int i;
-    char *tok = strtok(cmd_buffer, " ");
-    for (i = 0; i < MAX_ARGS && tok != NULL; ++i) {
-      cmd_tokens[i] = (char*)malloc(strlen(tok) * sizeof(char));
-      strcpy(cmd_tokens[i], tok);
-      tok = strtok(NULL, " ");
-    }
-    
-    i = 0; 
-  }
+    // tokenize by pipe
+    char *tok = strtok(cmd_buffer, "|");
+    for (i = 0; tok != NULL; ++i) {
+      tokens[i] = (char *)malloc(strlen(tok) * sizeof(char));
+      strcpy(tokens[i], tok);
 
-  exit(EXIT_SUCCESS);
+      tok = strtok(NULL, "|");
+    }
+
+    token_c = i;
+
+    char **args;
+    int arg_c = 0;
+    
+    for (i = 0; i < token_c - 1; ++i) {
+      // wipe
+      for (j = 0; j < arg_c; ++j)
+        free(args[j]);
+      arg_c = 0;
+
+      // tokenize by space
+      char *tok = strtok(tokens[i], " ");
+      for (j = 0; tok != NULL && j < MAX_ARGS; ++j) {
+        args[j] = (char *)malloc(strlen(tok) * sizeof(char));
+        strcpy(args[j], tok);
+
+        tok = strtok(NULL, " ");
+      }
+
+      arg_c = j;
+
+      printf("running: ");
+      for (j = 0; j < arg_c; ++j)
+        printf("%s ", args[j]);
+      putchar('\n');
+      
+      int pipefd[2];
+      pipe(pipefd);
+      
+      int pid = fork();
+      if (pid == 0){
+        dup2(pipefd[1], STDOUT_FILENO);
+        
+        execvp(args[0], args);
+
+        perror("exec");
+        exit(EXIT_FAILURE);
+      }
+
+      dup2(pipefd[0], STDIN_FILENO);
+      close(pipefd[1]);
+    }
+   
+    execvp(args[0], args);
+
+    perror("exec");
+    exit(EXIT_FAILURE);
+  }
 }
