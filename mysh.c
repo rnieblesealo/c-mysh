e080d77794d91b24712df88e3cbf47b9343df4c4
Rafael Niebles <rnieblesealo@gmail.com>
2024-10-22 08:50:42 -0400

    initial commit

--- mysh.c
+++ mysh.c
@@ -8,105 +8,27 @@
 #define MAX_ARGS 32
 #define MAX_PIPES 3
 
-void free_args(char *(*args)[MAX_ARGS]) {
-  printf("Freeing args...\n");
-
-  int i = 0, j = 0;
-  while (args[i][0] != NULL) {
-    while (args[i][j] != NULL) {
-      printf("Freeing %d %d...\n", i, j);
-
-      free(args[i][j]);
-      j++;
-    }
-
-    i = i + 1;
-    j = 0;
-  }
-}
-
 int main() {
   char cmd_buffer[BUF_SIZE];
-  char *(*args)[MAX_ARGS];
-  char *tok;
-
-  int fd1[2], fd2[2];
-  int pid1, pid2, pid3;
-
+  char *cmd_tokens[MAX_ARGS];
+  
   while (1) {
-    // show prompt
     printf("$ ");
 
-    // read stdin, removing any newlines
+    // get whole command
     fgets(cmd_buffer, BUF_SIZE, stdin);
     cmd_buffer[strcspn(cmd_buffer, "\n")] = '\0';
 
-    // tokenize by spaces
-    tok = strtok(cmd_buffer, " ");
-
-    int i = 0; // command
-    int j = 0; // individual arg
-
-    while (tok != NULL && j < MAX_ARGS) {
-      // handle exit
-      if (strcmp(tok, "exit") == 0)
-        break;
-
-      // if hit pipe token, null-terminate this args array and move to next
-      if (strcmp(tok, "|") == 0) {
-        args[i][j] = NULL;
-
-        i = i + 1;
-        j = 0;
-      }
-
-      else {
-        // add arg
-        printf("Allocing %d %d...\n", i, j);
-
-        args[i][j] = (char *)malloc(strlen(tok));
-        strncpy(args[i][j], tok, strlen(tok));
-
-        // move to next slot
-        j++;
-      }
-
-      // move token 
+    // tokenize by space
+    int i;
+    char *tok = strtok(cmd_buffer, " ");
+    for (i = 0; i < MAX_ARGS && tok != NULL; ++i) {
+      cmd_tokens[i] = (char*)malloc(strlen(tok) * sizeof(char));
+      strcpy(cmd_tokens[i], tok);
       tok = strtok(NULL, " ");
     }
-
-    // nullterm last arg
-    args[i][j] = NULL;
-
-    i++;
-
-    // first unused arg slot begins in null
-    args[i][0] = NULL;
-
-    // if used cd, change working dir to passed one
-    if (strcmp(args[0][0], "cd") == 0) {
-      if (chdir(args[0][1]) == -1) {
-        perror("chdir");
-      }
-    }
-
-    else {
-      // run process
-      pid1 = fork();
-      if (pid1 == 0) {
-        if (execvp(args[0][0], args[0]) == -1) {
-          perror("execvp");
-        }
-      }
-
-      else {
-        // if parent, await completion
-        wait(NULL);
-        
-        // once done with everything, this run, free args
-        free_args(args);
-      }
-    }
+    
+    i = 0; 
   }
 
   exit(EXIT_SUCCESS);
